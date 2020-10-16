e1374042ed7b512bbb7cf9f7fd99d7b44cff45b1
Amit <amitkumar.developer1@gmail.com>
2020-10-16 23:25:03 +0530

    Constant

---
+++ Constant.java
@@ -0,0 +1,7 @@
+public class Constant {
+    public static void main(String[] args) {
+        final float pi = 3.14F;
+        System.out.println(pi);
+        // pi = 4; It is commented as it will give an error
+    }
+}
