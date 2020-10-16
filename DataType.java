435a1389ec769cdd493df67c6bb328dbb2dd4ae1
Amit <amitkumar.developer1@gmail.com>
2020-10-16 22:53:47 +0530

    Data Types

---
+++ DataType.java
@@ -0,0 +1,12 @@
+public class DataType {
+    public static void main(String[] args) {
+        byte age = 22;
+        float height = 6.1F;
+        long viewCount = 3_345_123_999L;
+        boolean isEligible = true;
+        System.out.println(age);
+        System.out.println(height * 12);
+        System.out.println(viewCount);
+        System.out.println(isEligible);
+    }
+}
